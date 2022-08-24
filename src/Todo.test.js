503e890af7b65204f401e09129a86aa32329f764
Malo de Poret <72134718+Malodeporet@users.noreply.github.com>
2022-08-24 14:40:21 +0200

    2 : Change a TODO state

--- src/Todo.test.js
+++ src/Todo.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Todo from './components/Todo'
 
@@ -7,6 +7,11 @@ test('should render todo component', () => {
     const TodoElement = screen.getByTestId('todo-1');
     expect(TodoElement).toBeInTheDocument();
     expect(TodoElement).toHaveTextContent('Acheter des fruits');
+    const ch1 = screen.getByTestId('checkbox-1');
+    expect(ch1.checked).toEqual(false);
+    fireEvent.click(ch1);
+    expect(ch1.checked).toEqual(true);
+
 });
 
 test('should render todo component 2', () => {
@@ -14,4 +19,8 @@ test('should render todo component 2', () => {
     const TodoElement = screen.getByTestId('todo-2');
     expect(TodoElement).toBeInTheDocument();
     expect(TodoElement).toHaveTextContent('Coder une application');
+    const ch2 = screen.getByTestId('checkbox-2');
+    expect(ch2.checked).toEqual(false)
+    fireEvent.click(ch2)
+    expect(ch2.checked).toEqual(true)
 });
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,30 +1,61 @@
 import React, { useState } from "react";
 
 const Todo = () => {
-  //State
+  // State
   const [todos, setTodos] = useState([
     { id: 1, todo: "Acheter des fruits", completed: false },
     { id: 2, todo: "Coder une application", completed: false },
     { id: 3, todo: "Relancer Sogeti", completed: false },
   ]);
 
-  // List of the Tasks to do
-  const myTodos = todos.map((todo) => {
-    return (
-      <li className="todo_item" key={todo.id} data-testid={`todo-${todo.id}`}>
-        {todo.todo}
-      </li>
+  // Action of the checkbox to change the state of a Todo to completed
+  const completeTask = (id) => {
+    setTodos(
+      todos.map((item) => {
+        if (item.id === id) {
+          console.log(item.id, "Completed!");
+          return { ...item, completed: !item.completed };
+        } else {
+          return item;
+        }
+      })
     );
-  });
+  };
+
+  // List of the Tasks to do
+  const myTodos = todos
+    .filter((todo) => todo.completed === false)
+    .map((todo) => {
+      return (
+        <li className="todo_item" key={todo.id} data-testid={`todo-${todo.id}`}>
+          {todo.todo}
+          <input
+            type="checkbox"
+            data-testid={`checkbox-${todo.id}`}
+            onClick={() => completeTask(todo.id)}
+          />
+        </li>
+      );
+    });
+
+  // Todo completed
+  const myTodosCompleted = todos
+    .filter((todo) => todo.completed === true)
+    .map((todo) => {
+      return (
+        <li className="todo_item completed" key={todo.id}>
+          {todo.todo}
+        </li>
+      );
+    });
 
   // Render
   return (
     <div className="todo">
-      <h1 className="todo_title">
-        {todos.length} {todos.length !== 1 ? "Todos" : "Todo"}
-      </h1>
+      <h1 className="todo_title">Todo List</h1>
       <div className="todo_form">
         <ul className="todo_list">{myTodos}</ul>
+        <ul className="todo_list-completed">{myTodosCompleted}</ul>
       </div>
     </div>
   );
